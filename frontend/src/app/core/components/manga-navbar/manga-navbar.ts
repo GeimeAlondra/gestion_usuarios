f928829d1ac79cf6b21bcafa0312c08fe8d6a33c
wendy97531234 <aa0722032023@unab.edu.sv>
2026-05-02 21:58:41 -0600

    fix: agregado mini menu de perfil

--- frontend/src/app/core/components/manga-navbar/manga-navbar.ts
+++ frontend/src/app/core/components/manga-navbar/manga-navbar.ts
@@ -3,31 +3,49 @@ import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
+
 @Component({
-  selector: 'app-manga-navbar',
-  standalone: true,
-  imports: [CommonModule, RouterModule],
-  templateUrl: './manga-navbar.html',
-  styleUrls: ['./manga-navbar.css'],
+ selector: 'app-manga-navbar',
+ standalone: true,
+ imports: [
+   CommonModule,
+   RouterModule
+ ],
+ templateUrl: './manga-navbar.html',
+ styleUrls: ['./manga-navbar.css']
 })
 export class MangaNavbarComponent {
-  private authService = inject(AuthService);
-  private router = inject(Router);
 
-  user = this.authService.getUser();
 
-  logout(): void {
-    localStorage.removeItem('token');
-    localStorage.removeItem('user');
+ private authService = inject(AuthService);
+ private router = inject(Router);
 
-    this.router.navigate(['/login']);
-  }
 
-  isAdmin(): boolean {
-    return this.user?.role === 'Admin';
-  }
+ user = this.authService.getUser();
 
-  isEditor(): boolean {
-    return this.user?.role === 'Editor';
-  }
+
+ logout(): void {
+
+
+   localStorage.removeItem('token');
+   localStorage.removeItem('user');
+   this.router.navigate(['/login']);
+
+
+ }
+
+
+ isAdmin(): boolean {
+   return this.user?.role === 'Admin';
+ }
+
+
+ isEditor(): boolean {
+   return this.user?.role === 'Editor';
+ }
+
+
 }
+
+
+
